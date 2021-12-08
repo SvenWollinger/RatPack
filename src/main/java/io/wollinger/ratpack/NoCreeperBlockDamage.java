225b66bb9c1d4ce7d0bb475e6a797623368a901f
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-12-08 19:02:00 +0100

    Added sound and particle to NoCreeperBlockDamage

--- src/main/java/io/wollinger/ratpack/NoCreeperBlockDamage.java
+++ src/main/java/io/wollinger/ratpack/NoCreeperBlockDamage.java
@@ -1,5 +1,10 @@
 package io.wollinger.ratpack;
 
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,9 +12,14 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class NoCreeperBlockDamage implements Listener {
     @EventHandler
-    public void onCreeperExplosion(final EntityExplodeEvent e) {
-        if (e.getEntityType().equals(EntityType.CREEPER)) {
-            e.setCancelled(true);
+    public void onCreeperExplosion(final EntityExplodeEvent event) {
+        if (event.getEntityType().equals(EntityType.CREEPER)) {
+            event.setCancelled(true);
+            Entity entity = event.getEntity();
+            World world = entity.getWorld();
+            Location location = entity.getLocation();
+            world.playSound(location, "entity.generic.explode", 1F, 1F);
+            world.spawnParticle(Particle.SMOKE_LARGE, location, 1);
         }
     }
 }
