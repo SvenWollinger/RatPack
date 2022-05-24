7955b2a7172f6795b5191309b3e91f258ada8a11
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-05-24 14:57:20 +0200

    Initial create test

--- src/main/java/io/wollinger/ratpack/commands/WaypointCommand.java
+++ src/main/java/io/wollinger/ratpack/commands/WaypointCommand.java
@@ -1,12 +1,58 @@
 package io.wollinger.ratpack.commands;
 
+import io.wollinger.ratpack.features.waypoints.Waypoint;
+import io.wollinger.ratpack.features.waypoints.WaypointManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class WaypointCommand implements CommandBase {
+    private final static String USAGE = "Usage:";
+    private final static String USAGE_CREATE =  "/waypoint create <id> <label>";
+    private final static String USAGE_DELETE =  "/waypoint delete <id>";
+    private final static String USAGE_EDIT =    "/waypoint edit <id> (label|id) <input>";
+    private final static String USAGE_EXPORT =  "/waypoint export bluemap";
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(args.length == 0 || args[0].equals("help") || args[0].equals("?")) {
+            sender.sendMessage(USAGE, USAGE_CREATE, USAGE_DELETE, USAGE_EDIT, USAGE_EXPORT);
+            return true;
+        }
+
+
+        switch(args[0]) {
+            case "create":
+                create(args, (Player)sender);
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            case "export":
+                break;
+
+        }
+
+
         return true;
+    }
+
+    private void create(String[] args, Player player) {
+        if(args.length <= 2) {
+            player.sendMessage(USAGE, USAGE_CREATE);
+            return;
+        }
+        String id = args[1];
+        StringBuilder label = new StringBuilder();
+        for(int i = 2; i < args.length; i++) {
+            label.append(args[i]);
+            if(i != args.length - 1)
+                label.append(" ");
+        }
+
+        WaypointManager.addWaypoint(new Waypoint(id, label.toString(), player.getLocation()));
+        player.sendMessage(String.format("Waypoint %s created!", label));
     }
 
     @Override
--- src/main/java/io/wollinger/ratpack/features/waypoints/Waypoint.java
+++ src/main/java/io/wollinger/ratpack/features/waypoints/Waypoint.java
@@ -5,7 +5,7 @@ import org.bukkit.Location;
 public class Waypoint {
     private String id; //Allow changing of the id, storing the old and new in order to change it when saving
     private String label;
-    private Location location;
+    private Location location; //Make sure to save the world!!! If we just manage to load this location properly we wont need much more to set the compass
 
     public Waypoint(String id, String label, Location location) {
         this.id = id;
